8faa07af0a45c9030654850a0c247a51e633bf11
Jakub Klonowski <67651404+kapitanjaszczur@users.noreply.github.com>
2022-04-06 13:28:43 +0200

    klient2

--- Klient.Designer.cs
+++ Klient.Designer.cs
@@ -37,10 +37,9 @@ namespace ADO
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
@@ -48,12 +47,13 @@ namespace ADO
             this.label7 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
@@ -71,16 +71,16 @@ namespace ADO
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 39);
+            this.textBox1.Location = new System.Drawing.Point(139, 39);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(99, 91);
+            this.textBox2.Location = new System.Drawing.Point(139, 91);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(186, 20);
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
             this.textBox2.TabIndex = 3;
             // 
             // label1
@@ -88,9 +88,9 @@ namespace ADO
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Nazwa";
+            this.label1.Text = "Nazwa (10+ znaków)";
             // 
             // label2
             // 
@@ -106,9 +106,9 @@ namespace ADO
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(6, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.Size = new System.Drawing.Size(131, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Adres e-mail";
+            this.label3.Text = "Adres e-mail (10+ znaków)";
             // 
             // button1
             // 
@@ -122,7 +122,7 @@ namespace ADO
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label1);
@@ -137,19 +137,6 @@ namespace ADO
             this.groupBox1.Text = "Dodaj klienta";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(99, 65);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            999999999,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(186, 20);
-            this.numericUpDown1.TabIndex = 8;
-            this.numericUpDown1.ThousandsSeparator = true;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -161,8 +148,9 @@ namespace ADO
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.maskedTextBox2);
+            this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.numericUpDown3);
-            this.groupBox2.Controls.Add(this.numericUpDown2);
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.label4);
@@ -178,18 +166,17 @@ namespace ADO
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Modyfikuj/usuń klienta";
             // 
-            // numericUpDown2
+            // numericUpDown3
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(99, 78);
-            this.numericUpDown2.Maximum = new decimal(new int[] {
-            999999999,
+            this.numericUpDown3.Location = new System.Drawing.Point(139, 27);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            100000,
             0,
             0,
             0});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(193, 20);
-            this.numericUpDown2.TabIndex = 9;
-            this.numericUpDown2.ThousandsSeparator = true;
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(153, 20);
+            this.numericUpDown3.TabIndex = 14;
             // 
             // button3
             // 
@@ -216,9 +203,9 @@ namespace ADO
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(6, 55);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(107, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Nazwa";
+            this.label5.Text = "Nazwa (10+ znaków)";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
@@ -235,36 +222,66 @@ namespace ADO
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(6, 107);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.Size = new System.Drawing.Size(131, 13);
             this.label7.TabIndex = 6;
-            this.label7.Text = "Adres e-mail";
+            this.label7.Text = "Adres e-mail (10+ znaków)";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(99, 52);
+            this.textBox3.Location = new System.Drawing.Point(139, 52);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(193, 20);
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
             this.textBox3.TabIndex = 1;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(99, 104);
+            this.textBox4.Location = new System.Drawing.Point(139, 104);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(193, 20);
+            this.textBox4.Size = new System.Drawing.Size(153, 20);
             this.textBox4.TabIndex = 3;
             // 
-            // numericUpDown3
+            // button4
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(99, 27);
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(193, 20);
-            this.numericUpDown3.TabIndex = 14;
+            this.button4.Location = new System.Drawing.Point(527, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 25);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Odśwież";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(139, 65);
+            this.maskedTextBox1.Mask = "000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(146, 20);
+            this.maskedTextBox1.TabIndex = 9;
+            this.maskedTextBox1.Text = "000000000";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(139, 78);
+            this.maskedTextBox2.Mask = "000000000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(153, 20);
+            this.maskedTextBox2.TabIndex = 10;
+            this.maskedTextBox2.Text = "000000000";
             // 
             // Klient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(619, 347);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
@@ -274,10 +291,8 @@ namespace ADO
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
 
@@ -302,8 +317,10 @@ namespace ADO
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
     }
 }
--- Klient.cs
+++ Klient.cs
@@ -19,24 +19,11 @@ namespace ADO
             return _instance;
         }
 
-        string connectionString = "Server=DESKTOP-JH1VST5\\SQLEXPRESS;Database=BT;Uid=DESKTOP-JH1VST5\\N;Pwd=;Trusted_Connection=True;";
+        readonly string connectionString = "Server=DESKTOP-JH1VST5\\SQLEXPRESS;Database=BT;Uid=DESKTOP-JH1VST5\\N;Pwd=;Trusted_Connection=True;";
 
         private void Klient_Load(object sender, EventArgs e)
         {
-            SqlConnection connection = new SqlConnection(connectionString);
-            var dataAdapter = new SqlDataAdapter("SELECT * FROM dbo.Klient", connection);
-
-            try
-            {
-                var commandBuilder = new SqlCommandBuilder(dataAdapter);
-                var ds = new DataSet();
-                dataAdapter.Fill(ds);
-                dataGridView1.DataSource = ds.Tables[0];
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -57,21 +44,38 @@ namespace ADO
         private void button1_Click(object sender, EventArgs e) //dodawanie
         {
             string nazwa = textBox1.Text;
-            decimal telefon = numericUpDown1.Value;
+            int telefon = 0;
+            if (maskedTextBox1.Text == null)
+            {
+                MessageBox.Show("Wprowadź poprawne wartości");
+            }
+            else if (int.Parse(maskedTextBox1.Text) <1) {
+                MessageBox.Show("Wprowadź poprawne wartości");
+            }
+            else
+            {
+                telefon = int.Parse(maskedTextBox1.Text);
+            }
             string mail = textBox2.Text;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                if (nazwa.Length >10 && telefon >0 && mail.Length >10) {
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("INSERT INTO Klient VALUES ('" + nazwa + "', " + telefon + ", '" + mail + "');", connection);
+                        //connection.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wprowadź poprawne wartości");
+                }
                 
-                try
-                {
-                    SqlCommand cmd = new SqlCommand("INSERT INTO Klient VALUES ('"+nazwa+"', "+telefon+", '"+mail+"');", connection);
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
             }
 
         }
@@ -80,26 +84,46 @@ namespace ADO
         {
             decimal id = numericUpDown3.Value;
             string nazwa = textBox3.Text;
-            decimal telefon = numericUpDown2.Value;
+            int telefon = 0;
+            if (maskedTextBox2.Text == null)
+            {
+                MessageBox.Show("Wprowadź poprawne wartości");
+            }
+            else if (int.Parse(maskedTextBox2.Text) < 1)
+            {
+                MessageBox.Show("Wprowadź poprawne wartości");
+            }
+            else
+            {
+                telefon = int.Parse(maskedTextBox2.Text);
+            }
             string mail = textBox4.Text;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                try
+
+                if (nazwa.Length >10 && telefon >0 && mail.Length >10)
                 {
-                    SqlCommand cmd = new SqlCommand("UPDATE Klient " + "SET nazwa='" + nazwa + "', telefon=" + telefon + ", adres_mail='" + mail + "'" + "WHERE ID_klient=" + id, connection);
-                    connection.Open();
-                    cmd.ExecuteNonQuery();
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("UPDATE Klient " + "SET nazwa='" + nazwa + "', telefon=" + telefon + ", adres_mail='" + mail + "'" + "WHERE ID_klient=" + id, connection);
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show(ex.Message);
+                    MessageBox.Show("Wprowadź poprawne wartości");
                 }
             }
         }
 
         private void button3_Click(object sender, EventArgs e) //usuwanie
         {
-            decimal id = numericUpDown3.Value;
+            /*decimal id = numericUpDown3.Value; //usuń po id
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
@@ -112,6 +136,50 @@ namespace ADO
                 {
                     MessageBox.Show(ex.Message);
                 }
+            }*/
+
+            string cb = comboBox1.SelectedItem.ToString(); //usuń po nazwie
+            using (SqlConnection connection = new SqlConnection(connectionString))
+                try
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM Klient WHERE nazwa='" + cb + "'", connection);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //odśwież grid i combobox
+        {
+            SqlConnection connection = new SqlConnection(connectionString);//odśwież grid
+            var dataAdapter = new SqlDataAdapter("SELECT * FROM dbo.Klient", connection);
+            try
+            {
+                var commandBuilder = new SqlCommandBuilder(dataAdapter);
+                var ds = new DataSet();
+                dataAdapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))//odśwież combobox
+            {
+                SqlCommand sqlCmd = new SqlCommand("SELECT ID_klient, nazwa FROM dbo.Klient", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader sqlReader = sqlCmd.ExecuteReader();
+                comboBox1.Items.Clear();
+                while (sqlReader.Read())
+                {
+                    comboBox1.Items.Add(sqlReader["nazwa"].ToString());
+                }
+
+                sqlReader.Close();
             }
         }
     }
